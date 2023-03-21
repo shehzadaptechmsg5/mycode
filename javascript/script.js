c54fde9fe6903d1dbc66714416a0c7ae65f0bf1d
shehzad <shehzad.aptechmsg5@gmail.com>
2023-03-21 11:47:32 +0500

    my animation

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+document.write("<h1>Hello world</h1>");
+
+document.write("<br>");
+document.write("Hello world");
+document.write("<br>");
+document.write("Hello world");
+document.write("<br>");
+document.write("Hello world");
+document.write("<br>");
+document.write("Hello world");
+document.write("<br>");
+document.write("Hello world");
